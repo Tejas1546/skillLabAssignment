4b86b3d5e9923fac0a20d092e8e2111499ef7b04
Tejas1546 <jshettytejas@gmail.com>
2024-02-17 20:22:19 +0530

    Update shop.jsx

--- Assignment 2/src/pages/shop/shop.jsx
+++ Assignment 2/src/pages/shop/shop.jsx
@@ -7,7 +7,7 @@ export const Shop = () => {
   return (
     <div className="shop">
       <div className="shopTitle">
-        <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+        <h1>Lorem, ipsum dolor.</h1>
       </div>
 
       <div className="products">
